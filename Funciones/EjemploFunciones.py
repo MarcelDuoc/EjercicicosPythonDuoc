2f8643ef75d79107c5c278451e8d3ffe9cd45e61
MarcelDuoc <mar.umanzor@duocuc.cl>
2026-05-30 11:34:22 -0400

    funciones en otro archivo

--- Funciones/EjemploFunciones.py
+++ Funciones/EjemploFunciones.py
@@ -1,32 +1,13 @@
-#funciones
-
-def SumarDosNumeros():
-    '''Esta funcion permite dumar dos números,
-    ingresados dentro de la función
-    '''
-
-    num1= int(input("Ingrese número 1: "))
-    num2= int(input("Ingrese número 2: "))
-
-    suma = num1 + num2
-
-    print(f"la suma de {num1} + {num2} es = {suma}")
-
-def sumar(a,b):
-    '''Esta función permite sumar dos números
-    ingresados por parámetros
-    '''
-
-    suma = a + b
-    return suma
-
+import FuncionesDetalles as fn
+#from FuncionesDetalles import sumar
+ 
 #linea ppal
-SumarDosNumeros()
+fn.SumarDosNumeros()
 
-res = sumar(4,5)
+res = fn.sumar(4,5)
 print(res)
 
-num1 = int(inout("Numero 1: "))
-num2 = int(inout("Numero 2: "))
-res = sumar(num1,num2)
+num1 = int(input("Numero 1: "))
+num2 = int(input("Numero 2: "))
+res = fn.sumar(num1,num2)
 print("La suma es: ",res)
