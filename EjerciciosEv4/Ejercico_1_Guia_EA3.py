cd463749f9c969b7e2af6bea45af0e07b8e526f8
MarcelDuoc <mar.umanzor@duocuc.cl>
2026-06-01 22:07:03 -0400

    Menu

---
+++ EjerciciosEv4/Ejercico_1_Guia_EA3.py
@@ -0,0 +1,18 @@
+def agregar_productos(productos):
+
+'''productos = {
+    "Mouse": [10, 15000],
+    "Teclado": [5, 25000],
+    "Monitor": [3, 180000]
+}'''
+
+productos = {} #diccionario vacío
+#productos = [] Lista
+while True:
+    print ("1. Agregar producto")
+    print ("2. Mostrar productos")
+    print ("3. Buscar productos")
+    print ("4. Producto mas caro")
+    print ("5. Salir")
+
+    
