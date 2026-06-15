bcec760b4e918210bf1c479426ac8ff59bdf08a7
Your Name <you@example.com>
2026-06-15 00:17:34 -0400

    Funciones agregar y mostrar productos

--- EjerciciosEv4/Ejercico_1_Guia_EA3.py
+++ EjerciciosEv4/Ejercico_1_Guia_EA3.py
@@ -4,10 +4,31 @@
     "Monitor": [3, 180000]
 }'''
 
-#productos = [] Lista
-
 def agregar_productos(productos):
-    nombre
+    nombre = input("Nombre del producto: ").strip().lower()
+
+    if nombre == "":
+        print("El nombre no puede ser vacío")
+        return
+    if nombre in productos:
+        print("El producto ya existe!")
+        return
+    
+    stock = int(input("Ingrese Stock: "))
+    precio = int(input("Ingrese precio $: "))
+
+    productos [nombre] = [stock,precio]
+    print ("Producto agregado correctamente!")
+
+def mostrar_productos(productos):
+    if len(productos) == 0:
+        print("No existen productos")
+        return
+    for nombre in productos:
+        print(nombre,"-Stock: ",productos[nombre][0],"-Precio: $",productos[nombre][1])
+
+#def buscar_productos(productos):
+#def producto_mas_caro(productos):
 
 # codigo ppal
 
@@ -24,23 +45,24 @@ while True:
 
     while True:
         try:
-            op = int(input("Ingrese la opción: +"))
+            op = int(input("Ingrese la opción: "))
             break
         except ValueError:
             print ("Debe ingresar una opción válidad (1-5, intente nuevamente)")
 
     if op == 1:
         print ("Agregar Producto")
+        agregar_productos(productos)
 
     elif op == 2:
         print ("Mostrar Productos")
-
+        mostrar_productos(productos)
     elif op == 3:
         print ("Buscar Productos")
-
+        #buscar_productos(productos):
     elif op ==4:
         print ("Producto más caro")
-
+        #producto_mas_caro(producto):
     elif op ==5:
         print ("Fin del programa")
         break
@@ -48,5 +70,3 @@ while True:
     else:
         print ("Opción no válida")
 
-
-    
