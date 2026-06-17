08b9b227e8ae9742fdecf4e5c78208d22dc3901b
Your Name <you@example.com>
2026-06-17 13:57:08 -0400

    Validaciones en Stock y Precio listas

--- EjerciciosEv4/Ejercico_1_Guia_EA3.py
+++ EjerciciosEv4/Ejercico_1_Guia_EA3.py
@@ -13,9 +13,28 @@ def agregar_productos(productos):
     if nombre in productos:
         print("El producto ya existe!")
         return
+    if nombre.isdigit():
+        print("El nombre no puede ser un digito")
+        return
     
-    stock = int(input("Ingrese Stock: "))
-    precio = int(input("Ingrese precio $: "))
+    while True:
+            try:
+                stock = int(input("Ingrese Stock: "))
+                if stock >= 0:
+                    break
+                else:
+                    print("El stock debe ser mayor o igual a 0")
+            except ValueError:
+                print("El Stock, debe ser un número entero mayor o igual a 0")
+    while True:
+        try:
+            precio = int(input("Ingrese precio $: "))
+            if precio >0:
+                break
+            else:
+                print("El precio debe ser mayor que 0")
+        except ValueError:
+            print("El Precio, debe un número entero mayor que 0")
 
     productos [nombre] = [stock,precio]
     print ("Producto agregado correctamente!")
@@ -27,8 +46,36 @@ def mostrar_productos(productos):
     for nombre in productos:
         print(nombre,"-Stock: ",productos[nombre][0],"-Precio: $",productos[nombre][1])
 
-#def buscar_productos(productos):
-#def producto_mas_caro(productos):
+def buscar_productos(productos):
+    if len(productos) == 0:
+        print("No existen productos")
+        return
+    nombre = input("Ingrese nombre del producyo a buscar: ").strip().lower()
+
+    if nombre in productos:
+        print(f"Producto {nombre} encontrado")
+        print(f"Stock: {productos[nombre][0]}")
+        print(f"Precio: {productos[nombre][1]}") 
+
+    else:
+        print("Producto no existe o esta agotado")
+        
+def producto_mas_caro(productos):
+    if len(productos) == 0:
+        print("No existen productos")
+        return
+    
+    mayor = 0
+    nombreMayor = ""
+    for nombre in productos:
+        precio = productos [nombre][1]
+
+        if precio > mayor:
+            mayor = precio
+            nombreMayor = nombre
+
+    print(f"producto mas caro es: {nombreMayor}")
+    print(f"Su precio es: ${mayor}")
 
 # codigo ppal
 
@@ -58,11 +105,13 @@ while True:
         print ("Mostrar Productos")
         mostrar_productos(productos)
     elif op == 3:
+        # for Key,Value in productos.items():
+        #     print (Key,Value)
         print ("Buscar Productos")
-        #buscar_productos(productos):
+        buscar_productos(productos)
     elif op ==4:
-        print ("Producto más caro")
-        #producto_mas_caro(producto):
+        #print ("Producto más caro")
+        producto_mas_caro(productos)
     elif op ==5:
         print ("Fin del programa")
         break
